6b28a65740bef1927b8e813b52f74629fdafffdf
Nabin Thapa <erewhonaden23@gmail.com>
2023-11-14 16:19:01 +0545

    Finished the basics

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -1,17 +1,19 @@
-import { getMovies } from "@/scrapper"
+import { getMovies } from "@/scrapper";
 import { URLParser } from "@/utils";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, res: NextResponse){
-    try{
-        let parser = new URLParser(req.url);
-        let page = parser.getParam("page");
-        let alph = parser.getParam("aph")?.toUpperCase();
-        if(!page) page = "1";
-        if(!alph) alph = "";
-        let response = await getMovies(alph, page);
-        return new Response(JSON.stringify(response), {status: 200});
-    }catch(e: any){
-        return new Response(JSON.stringify({message: e.message}), {status: 4044})
-    }
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    let parser = new URLParser(req.url);
+    let page = parser.getParam("page");
+    let alph = parser.getParam("aph")?.toUpperCase();
+    if (!page) page = "1";
+    if (!alph) alph = "";
+    let response = await getMovies(alph, page);
+    return new Response(JSON.stringify(response), { status: 200 });
+  } catch (e: any) {
+    return new Response(JSON.stringify({ message: e.message }), {
+      status: 404,
+    });
+  }
 }
