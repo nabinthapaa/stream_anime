0ec09addd269c1ee63d900cea6757dc716b1af04
Nabin Thapa <erewhonaden23@gmail.com>
2023-11-14 16:55:11 +0545

    Finished the basics

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,13 +1,13 @@
 import { search } from "@/scrapper";
+import { URLParser } from "@/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
-    console.log(req.url + "\t" + req.method + "\t");
-    const url = req.url ? new URL(req.url) : null;
-    if (url === null) return;
-    let name = url.searchParams.get("q") as string;
-    let page = Number(url.searchParams.get("page")) || 1;
+    let parser = new URLParser(req.url);
+    let name = parser.getParam("q") as string;
+    let page = Number(parser.getParam("page")) || 1;
+    if (!name) name = "naruto";
     let response = await search(name, page);
     return new Response(JSON.stringify(response), {
       status: 200,
